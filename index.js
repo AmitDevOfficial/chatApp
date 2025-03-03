a7baa5e7a4a67c20ca5149797664d9793ded100d
Amit <vishamit8827@gmail.com>
2025-03-03 17:43:00 +0530

    chat

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const {createServer} = require("node:http");
+const path = require("path");
+const { Server } = require('socket.io');
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
+
+//--Handel All Socket---//
+
+io.on("connection", (socket) => {
+    socket.on("user-msg", (msg) =>{
+        // console.log("A new user message", msg);
+        io.emit("msg", msg);
+    })
+})
+
+app.use(express.static(path.resolve("./public")))
+
+app.get("/", (req, res) => {
+    return res.sendFile("/public/index.html");
+})
+
+server.listen(3000, () => {
+    console.log(`server running at 3000`)
+})
+
+
